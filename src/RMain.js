8901012e0176225c248e68fa47e3a70ba7088732
Ryan Timmons <ryan@rytim.com>
2016-11-19 14:06:45 -0500

    Move things to RMain

---
+++ src/RMain.js
@@ -0,0 +1,70 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+var Bus = require('./Bus.js');
+var RGame = require('./RGame.js');
+
+class RMain {
+  constructor(params) {
+    this.memory = params.memory;
+    this.game   = params.game;
+    this.bus    = new Bus();
+    this.rgame  = new RGame();
+  }
+  tick() {
+
+    this.rgame.ensureCreepCount({
+      role: 'harvester',
+      atLeast: 1,
+      bodyParts: [WORK,CARRY,MOVE]
+    });
+    this.rgame.ensureCreepCount({
+      role: 'builder',
+      atLeast: 1,
+      bodyParts: [WORK,CARRY,MOVE]
+    });
+    this.rgame.ensureCreepCount({
+      role: 'upgrader',
+      atLeast: 3,
+      bodyParts: [WORK,CARRY,MOVE]
+    });
+
+    // To kill: Game.creeps['Harvester1'].suicide()
+    // Also: StructureSpawn.renewCreep - http://support.screeps.com/hc/en-us/articles/205990342-StructureSpawn#renewCreep
+
+    for(var name in this.memory.creeps) {
+      if(!this.memory.creeps[name]) {
+        delete this.memory.creeps[name];
+        console.log('Clearing non-existing creep memory:', name);
+      }
+    }
+
+    var tower = this.game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in this.game.creeps) {
+        var creep = this.game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,63 +1,9 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var RGame = require('RGame');
-
+var RMain = require('RMain');
 
 module.exports.loop = function () {
-
-    var rgame = new RGame();
-
-    rgame.ensureCreepCount({
-      role: 'harvester',
-      atLeast: 1,
-      bodyParts: [WORK,CARRY,MOVE]
+    var rmain = new RMain({
+      game: Game,
+      memory: Memory
     });
-    rgame.ensureCreepCount({
-      role: 'builder',
-      atLeast: 1,
-      bodyParts: [WORK,CARRY,MOVE]
-    });
-    rgame.ensureCreepCount({
-      role: 'upgrader',
-      atLeast: 3,
-      bodyParts: [WORK,CARRY,MOVE]
-    });
-
-    // To kill: Game.creeps['Harvester1'].suicide()
-    // Also: StructureSpawn.renewCreep - http://support.screeps.com/hc/en-us/articles/205990342-StructureSpawn#renewCreep
-
-    for(var name in Memory.creeps) {
-      if(!Game.creeps[name]) {
-        delete Memory.creeps[name];
-        console.log('Clearing non-existing creep memory:', name);
-      }
-    }
-
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-    }
+    rmain.tick();
 }
