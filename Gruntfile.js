35854e44e76cb1d2db7f606eb7cf0c486b0ac81f
Ryan Timmons <ryan@rytim.com>
2016-11-18 19:44:25 -0500

    start adding some tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-screeps');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.initConfig({
     screeps: {
@@ -13,6 +14,21 @@ module.exports = function(grunt) {
       dist: {
         src: ['src/*.js']
       }
-    }
+    },
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+        },
+        src: ['tst/spec/*.js']
+      }
+    },
   });
+
+  grunt.registerTask('default', 'mochaTest');
 }
---
+++ tst/spec/role.harvester.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+var roleHarvester = require('../../src/role.harvester.js');
+
+describe('harvester',() => {
+  it('moves to source if has capacity',() => {
+    // TODO
+  });
+});
