b747d21bd399c65309cd006d8c0c669852d27665
Ryan Timmons <ryan@rytim.com>
2016-11-19 18:30:30 -0500

    Use mode instead of boolean flags

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -1,18 +1,21 @@
+var Deco = require('DecoratedCreep');
+
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+      delete creep.memory.upgrading;
 
-        if(creep.memory.upgrading && creep.carry.energy <= 0) {
-            creep.memory.upgrading = false;
+        if(creep.memory.mode == 'upgrading' && creep.carry.energy <= 0) {
             creep.say('harvesting');
+            creep.memory.mode = 'harvesting';
         }
-        if(!creep.memory.upgrading && creep.carry.energy >= creep.carryCapacity) {
-            creep.memory.upgrading = true;
+        if(creep.memory.mode != 'upgrading' && creep.carry.energy >= creep.carryCapacity) {
             creep.say('upgrading');
+            creep.memory.mode = 'upgrading';
         }
 
-        if(creep.memory.upgrading) {
+        if(creep.memory.mode == 'upgrading') {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
