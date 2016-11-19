baca9eb4f29678060fa017a98aa651e7c21b3cd1
Ryan Timmons <ryan@rytim.com>
2016-11-18 19:32:13 -0500

    Copy from tutorial: auto-spawn harvesters

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
+    // https://screeps.com/a/#!/sim/tutorial/4
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+    if(harvesters.length < 2) {
+      var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined,
+        {role: 'harvester'});
+      console.log('Spawning new harvester: ' + newName);
+    }
+    // To kill: Game.creeps['Harvester1'].suicide()
+    // Also: StructureSpawn.renewCreep - http://support.screeps.com/hc/en-us/articles/205990342-StructureSpawn#renewCreep
+
+    for(var name in Memory.creeps) {
+      if(!Game.creeps[name]) {
+        delete Memory.creeps[name];
+        console.log('Clearing non-existing creep memory:', name);
+      }
+    }
+
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
