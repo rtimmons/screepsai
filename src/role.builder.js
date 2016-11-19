3642eb67ead0ffc62ef6e09931961d0f2c627f0f
Ryan Timmons <ryan@rytim.com>
2016-11-19 14:36:25 -0500

    Allow builders to upgrade room controllers if no construction sites

--- src/role.builder.js
+++ src/role.builder.js
@@ -9,15 +9,23 @@ var roleBuilder = {
         }
         if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
             creep.memory.building = true;
+            creep.memorgy.construction = true;
             creep.say('building');
         }
 
-        if(creep.memory.building) {
+        if(creep.memory.building && creep.memory.construction) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+                return;
+            }
+        }
+        else if(creep.memory.building) {
+            creep.memory.construction = false;
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
             }
         }
         else {
--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -3,11 +3,11 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+        if(creep.memory.upgrading && creep.carry.energy <= 0) {
             creep.memory.upgrading = false;
             creep.say('harvesting');
         }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+        if(!creep.memory.upgrading && creep.carry.energy >= creep.carryCapacity) {
             creep.memory.upgrading = true;
             creep.say('upgrading');
         }
