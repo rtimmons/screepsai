a23149c7b81a5a24e123b303790cd600c85d4025
Ryan Timmons <ryan@rytim.com>
2016-11-19 19:28:56 -0500

    Use new spawn extension

--- src/GameUtil.js
+++ src/GameUtil.js
@@ -5,5 +5,17 @@ module.exports = {
     clz.creep = function(creepName) {
       return new Deco(Game.creeps[creepName]);
     };
+    clz.report = function() {
+      var report = [];
+      for(var k in Game.creeps) {
+        var c = Game.creeps[k];
+        report.push(JSON.stringify({
+          role: c.memory.role, mode: c.memory.mode, 
+          body: c.body.map(b => b.type.substring(0,1)).sort().join(''), 
+          name: c.name
+        }));
+      }
+      console.log(report.join("\n"));
+    };
   }
 };
