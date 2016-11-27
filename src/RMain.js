d72c49f9f09b491120686821926f839fc396e445
Ryan Timmons <ryan@rytim.com>
2016-11-27 16:06:43 -0500

    Move towe-control to ontick

--- src/RMain.js
+++ src/RMain.js
@@ -9,19 +9,7 @@ class RMain {
     this.game   = params.game;
   }
 
-  controlTower(towerId) {
-    // TODO: move somewhere else
-    // TODO: repeat for all towers?
-    var tower = this.game.getObjectById(towerId);
-    if (tower) {
-      tower.onTick(Game.time);
-    }
-  }
-
   tick() {
-    this.controlTower('583276ecf3a0a9785e5e5fa3');
-    this.controlTower('58370345a19b6bfd464d5f97');
-
     for (var name in this.game.creeps) {
       var creep = this.game.creeps[name];
       creep.tick(Game.time);
---
+++ src/ontick/ontick.controlTowers.js
@@ -0,0 +1,15 @@
+module.exports = {
+  controlTower(towerId) {
+    // TODO: move somewhere else
+    // TODO: repeat for all towers?
+    var tower = this.game.getObjectById(towerId);
+    if (tower) {
+      tower.onTick(Game.time);
+    }
+  },
+
+  onTick(context) {
+    this.controlTower('583276ecf3a0a9785e5e5fa3');
+    this.controlTower('58370345a19b6bfd464d5f97');
+  }
+};
